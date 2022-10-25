2a347d680650a4498a3031f2811eb9a2e1bee5b9
natnichapin <natnicha.siri@kmutt.ac.th>
2022-10-25 10:12:40 +0700

    Initial commit

---
+++ src/main/java/Servlet/product_list_021Servlet.java
@@ -0,0 +1,18 @@
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "product_list_021Servlet", value = "/product_list_021Servlet")
+public class product_list_021Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
